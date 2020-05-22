52ad3d7ee92f2ae53088d886d26d2348ec1214d2
andrewhpark25 <andrewhpark25@gmail.com>
2020-05-22 08:46:19 -0700

    MVP

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,73 @@
+const server = require('../api/server.js');
+
+const supertest = require('supertest');
+
+
+
+
+describe('router', () => {
+    it('can run the tests', () => {
+        expect(true).toBeTruthy();
+    })
+
+     
+         describe('POST /register', () => {
+            it('should return status 201', async () => {
+
+                const response = await supertest(server).post('/api/auth/register').send({username:"ronaldo", password:"juventus"})
+
+                 expect(response.status).toBe(201);
+              
+                
+              }) 
+             
+         })
+
+         describe('POST /register', () => {
+            it('return status 500 if nothing is sent', async () => {
+
+                const response = await supertest(server).post('/api/auth/register')
+
+                 expect(response.status).toBe(400);
+              
+              
+              }) 
+             
+         })
+
+         
+        describe('POST /login', () => {
+            it('should return status 200', async () => {
+
+                const response = await  supertest(server).post('/api/auth/login').send({username:"ronaldo", password:"juventus"})
+
+                 expect(response.status).toBe(200);
+              
+                
+              }) 
+             
+         })
+         describe('POST /login', () => {
+            it('should return status 401 if not authenticated user', async () => {
+
+                const response = await  supertest(server).post('/api/auth/login').send({username:"someone", password:"new123"})
+
+                 expect(response.status).toBe(401);
+              
+                
+              }) 
+             
+         })
+
+         describe('GET /logout', () => {
+            it('should return status 204', () => {
+                return (
+                supertest(server).get('/api/auth/logout').then(response => {
+                    expect(response.status).toBe(204);
+                })
+                );
+              }) 
+         })
+         
+   
+})
